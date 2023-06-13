dc4d032112f6aa98dea4145c9d4b631ecbef5a83
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-13 16:33:38 +0200

    Add functionality for getting all users

---
+++ MovieSystemNewest1/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using MovieSystemNewest1.Data;
+
+namespace MovieSystemNewest1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+            {
+                builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+            }));
+
+            builder.Services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors("corsapp");
+            app.UseAuthorization();
+
+
+            //Geting all users
+            app.MapGet("/Get/User", async (DataContext context) =>
+            {
+                var user = context.User;
+                return await user.ToListAsync();
+            })
+           .WithName("GetUser");
+
+
+            app.Run();
+        }
+    }
+}
