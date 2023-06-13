04b5464d6ebfb0ac5825284c65d71bd7e0d5b7f6
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-13 16:29:06 +0200

    Add user model class

---
+++ MovieSystemNewest1/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieSystemNewest1.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string? Email { get; set; }
+
+        public List<UserGenre> UserGenres { get; set; }
+    }
+}
+
