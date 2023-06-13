3191d59556c1f1bbf0cb46c333fcb5183a8776e3
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-13 16:30:40 +0200

    Add user genre model class

---
+++ MovieSystemNewest1/Model/UserGenre.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieSystemNewest1.Model
+
+{
+    public class UserGenre
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public string? Movie { get; set; }
+        public int? Rating { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+
+   
+    }
+}
+
