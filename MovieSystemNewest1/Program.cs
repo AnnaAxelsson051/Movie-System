4549b0a69679ceb7f274b49c5bf872b2913447c5
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-14 14:36:42 +0200

    Modify json, modify variable names in program and add functionality for adding user rating to a movie

--- MovieSystemNewest1/Program.cs
+++ MovieSystemNewest1/Program.cs
@@ -53,28 +53,28 @@ namespace MovieSystemNewest1
             //Getting genre for specific user
             app.MapGet("/Get/UserGenre/", async (int Id, DataContext context) =>
             {
-                var userGenre = from userGenreItem in context.UserGenre
+                var userGenre = from UserGenre in context.UserGenre
                                 select new
                                 {
-                                    userGenreItem.User.Id,
-                                    userGenreItem.Genre.Title
+                                    UserGenre.User.Id,
+                                    UserGenre.Genre.Title
                                 };
 
-                return await userGenre.Where(userGenreItem => userGenreItem.Id == Id).ToListAsync();
+                return await userGenre.Where(UserGenre => UserGenre.Id == Id).ToListAsync();
             })
             .WithName("/Get/GenrebyUserId");
 
             //Getting movies for specific user
             app.MapGet("/Get/UserMovie/", async (int Id, DataContext context) =>
             {
-                var userMovie = from userGenreItem in context.UserGenre
+                var userMovie = from UserGenre in context.UserGenre
                                 select new
                                 {
-                                    userGenreItem.User.Id,
-                                    userGenreItem.Movie
+                                    UserGenre.User.Id,
+                                    UserGenre.Movie
                                 };
 
-                return await userMovie.Where(userGenreItem => userGenreItem.Id == Id).ToListAsync();
+                return await userMovie.Where(UserGenre => UserGenre.Id == Id).ToListAsync();
             })
             .WithName("GetMoviebyUserId");
 
@@ -83,17 +83,29 @@ namespace MovieSystemNewest1
 
             app.MapGet("/Get/MoviesRating/", async (int Id, DataContext context) =>
             {
-                var movieRating = from userGenreItem in context.UserGenre
+                var movieRating = from UserGenre in context.UserGenre
                                   select new
                                   {
-                                      userGenreItem.User.Id,
-                                      userGenreItem.Movie,
-                                      userGenreItem.Rating
+                                      UserGenre.User.Id,
+                                      UserGenre.Movie,
+                                      UserGenre.Rating
                                   };
 
-                return await movieRating.Where(userGenreItem => userGenreItem.Id == Id).ToListAsync();
+                return await movieRating.Where(UserGenre => UserGenre.Id == Id).ToListAsync();
             })
             .WithName("GetMovieRatingbyUserId");
+
+            //Add rating from a specific user on a specific movie 
+
+            app.MapPost("/Post/AddRating/", async (DataContext context, int userId, int rating, string movie) =>
+            {
+                var updateRows = await context.UserGenre.Where(UserGenre => UserGenre.UserId == userId).Where(x => x.Movie == movie)
+                .ExecuteUpdateAsync(updates =>
+                updates.SetProperty(UserGenre => UserGenre.Rating, rating));
+
+                return updateRows == 0 ? Results.NotFound() : Results.NoContent();
+            })
+               .WithName("PostRatingByUserIdAndMovieId");
 
             app.Run();
         }
